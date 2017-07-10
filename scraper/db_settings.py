6e2625aafc98495f9236ff2b4b0b999893d76f08
Colin Burr <colin.a.burr@gmail.com>
2017-07-10 13:40:58 -0400

    change url

--- scraper/db_settings.py
+++ scraper/db_settings.py
@@ -5,6 +5,6 @@ mysql_host = '127.0.0.1'
 mysql_port = 30002
 mysql_pw = None
 
-site_url = 'http://www.socialmediatoday.com/'
+site_url = 'https://www---smt-import-20170710-vt6syoq-3faqimkzadf6s.us.platform.sh/'
 
 execution_path = '/Users/bmorin/workspace/smt/smt-scrapy'
