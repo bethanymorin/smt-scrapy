db37eade725c07ac6e2ae182b969f6911c86bcc9
Colin Burr <colin.a.burr@gmail.com>
2017-07-10 13:42:03 -0400

    change url

--- scraper/db_settings.py
+++ scraper/db_settings.py
@@ -1,10 +1,10 @@
 # Database settings
-mysql_user = 'user'
+mysql_user = 'colin'
 mysql_db = 'main'
 mysql_host = '127.0.0.1'
-mysql_port = 30002
-mysql_pw = None
+mysql_port = 3306
+mysql_pw = 'changeme'
 
 site_url = 'https://www---smt-import-20170710-vt6syoq-3faqimkzadf6s.us.platform.sh/'
 
-execution_path = '/Users/bmorin/workspace/smt/smt-scrapy'
+execution_path = '/home/colin/smt-scrapy'
