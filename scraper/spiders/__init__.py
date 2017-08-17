83c4626c5419a428afbb172763f1ea224d3113b3
Josh Brown <josh.s.brown@hotmail.com>
2017-08-17 17:48:07 -0400

    Fix incorrect css selector paths (maybe)

--- scraper/spiders/__init__.py
+++ scraper/spiders/__init__.py
@@ -164,6 +164,26 @@ class SocialMediaToday(scrapy.Spider):
             if field_value:
                 item[item_key] = field_value.strip()
 
+        bio = body.css('.field-name-field-user-biography div div::text').extract_first()
+        fullname = body.css('.field-name-user-full-name div div a strong::text').extract_first() + response.css(
+            '.field-name-user-full-name div div a::text'
+        ).extract_first()
+        job_title = body.css('.field-name-field-user-job-title div div.field-item::text').extract_first()
+        company_name = body.css('.field-name-field-user-company-name div.field-item::text').extract_first()
+        twitter = body.css('.field-name-field-user-twitter-url div div.field-item a::text').extract_first()
+        facebook = body.css('.field-name-field-user-facebook-url div div.field-item a::text').extract_first()
+        linkedin = body.css('.field-name-field-user-linkedin-url div div.field-item a::text').extract_first()
+        google = body.css('.field-name-field-user-google-url div div.field-item a::text').extract_first()
+
+        item['google'] = google.strip()
+        item['linkedin'] = linkedin.strip()
+        item['facebook'] = facebook.strip()
+        item['twitter'] = twitter.strip()
+        item['company_name'] = company_name.strip()
+        item['job_title'] = job_title.strip()
+        item['fullname'] = fullname.strip()
+        item['bio'] = bio.strip()
+
         headshot_url = body.css(
             'div.field-name-ds-user-picture img::attr(src)').extract_first()
         item['headshot_url'] = headshot_url or ''
